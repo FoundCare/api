706f94d9337f100bbb28cda4bbae537683332c0c
Jefferson Oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-09-11 20:21:50 -0300

    adicionado try-catch

--- app/Http/Controllers/Api/ProfissionalController.php
+++ app/Http/Controllers/Api/ProfissionalController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Profissional;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class ProfissionalController extends Controller
 {
@@ -71,6 +73,43 @@ class ProfissionalController extends Controller
 
     public function store(Request $request)
     {
+        /**
+         * {
+         *    "name": "Novo profissional",
+         *    "cpf": "00000000000",
+         *    "email": "jeff@gmail.com",
+         *    "data_nasc": "1999-11-29",
+         *    "logradouro": "Rua dos bobos n°0",
+         *    "bairro": "Diadema",
+         *    "cep": "09970-342",
+         *    "telefone": null,
+         *    "celular": "11988745706",
+         *    "cnpj": "05",
+         *    "razao_social": "FOUNDCARE",
+         *    "coren": "24158767"
+         *}
+         */
+        // Iniciar a transação
+        DB::beginTransaction();
+        dd($request->all());
+        try{
+            $profissional = Profissional::create([
+                "name" => $request->name
+            ]);
+
+        } catch(Exception $e){
+            // Operação não concluída com êxito
+            DB::rollBack();
+
+            $body = [
+                false, 
+                "Profissional não cadastrado", 
+                []
+            ];
+    
+            return $this->sendResponse($body, 403);
+        }
+
         return [
             "success" => "it's working!"
         ];
