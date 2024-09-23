4c1a59490f1de6ec606b9a5f70e94a1c2c507e92
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-09-23 11:53:36 -0300

    feat: feito o relacionamento 1:1 entre usuário e paciente

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
                 "cpf" => "47608923890",
                 "id_endereco" => 1,
                 'senha' => Hash::make('123456', ['rounds' => 12]),
-                "id_contato" => 5
+                "id_contato" => 1
             ]);
         }
         if(!User::where('email', 'ruansilva1404@gmail.com')->first()){
@@ -33,7 +33,7 @@ class UserSeeder extends Seeder
                 "cpf" => "47608923891",
                 "id_endereco" => 2,
                 'senha' => Hash::make('123456', ['rounds' => 12]),
-                "id_contato" => 5
+                "id_contato" => 2
             ]);
         }
         if(!User::where('email', 'geovannasilvasousa2@gmail.com')->first()){
@@ -44,7 +44,7 @@ class UserSeeder extends Seeder
                 "cpf" => "47608923892",
                 "id_endereco" => 3,
                 'senha' => Hash::make('123456', ['rounds' => 12]),
-                "id_contato" => 5
+                "id_contato" => 3
             ]);
         }
         if(!User::where('email', 'arthuralexandredealmeida@gmail.com')->first()){
@@ -55,7 +55,7 @@ class UserSeeder extends Seeder
                 "cpf" => "47608923893",
                 "id_endereco" => 4,
                 'senha' => Hash::make('123456', ['rounds' => 12]),
-                "id_contato" => 5
+                "id_contato" => 4
             ]);
         }
         if(!User::where('email', 'j.vitor.moura.37@gmail.com')->first()){
@@ -77,7 +77,7 @@ class UserSeeder extends Seeder
                 "cpf" => "47608923895",
                 "id_endereco" => 5,
                 'senha' => Hash::make('123456', ['rounds' => 12]),
-                "id_contato" => 5
+                "id_contato" => 6
             ]);
         }
     }
