bfb10204e6af81ffaddcaa6fde4c89f3d91b7aa7
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-09-24 11:51:47 -0300

    feat: definido relacionamento entre as tabelas, paciente, anuncio e historicoDeProfissionais

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Paciente extends Model
 {
@@ -13,5 +14,10 @@ class Paciente extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function historicoDeProfissionais(): HasMany
+    {
+        return $this->hasMany(HistoricoDeProfissional::class, "id_historico_profissional");
     }
 }
