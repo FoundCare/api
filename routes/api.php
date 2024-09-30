f1a56d1ca4d70d44aba03aa7e1e20abdf1a7fc91
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-09-30 13:37:08 -0300

    feat: adicionado as rotas para paciente

--- routes/api.php
+++ routes/api.php
@@ -2,44 +2,27 @@
 
 use App\Http\Controllers\Api\ContatoController;
 use App\Http\Controllers\Api\EnderecoController;
+use App\Http\Controllers\Api\PacienteController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProfissionalController;
 use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/enderecos/{id}', [EnderecoController::class, 'show']);
-Route::get('/contatos/{id}', [ContatoController::class, 'show']);
-Route::get('teste-teste', [ExampleController::class, 'teste']);
-
 /** ROTAS DOS USUÁRIOS */
-// GET - http://127.0.0.1:8000/api/users?page=1
 Route::get('/users', [UserController::class, 'index']);
-
-// GET - http://127.0.0.1:8000/api/users/{user}
 Route::get('/users/{id}', [UserController::class, 'show']);
-
-// POST - http://127.0.0.1:8000/api/users
 Route::post('/users', [UserController::class, 'store']);
-
-// PUT - http://127.0.0.1:8000/api/users/1
 Route::patch('/users/{id}', [UserController::class, 'update']);
-
-// DELETE - http://127.0.0.1:8000/api/users/1
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
+/**ROTAS DOS PACIENTES */
+Route::get("/pacientes", [PacienteController::class, 'index']);
+Route::get("/pacientes/{id}", [PacienteController::class, 'show']);
+Route::post("/pacientes", [PacienteController::class, 'store']);
+
 /** ROTAS DO PROFISSIONAL*/
-
-// GET http://127.0.0.1:8000/api/profissionais
 Route::get('/profissionais', [ProfissionalController::class, 'index']);
-
-// GET http://127.0.0.1:8000/api/profissionais/{profissional}
 Route::get('/profissionais/{profissional}', [ProfissionalController::class, 'show']);
-
-// POST http://127.0.0.1:8000/api/profissionais
 Route::post('/profissionais', [ProfissionalController::class, 'store']);
-
-// PATCH http://127.0.0.1:8000/api/profissionais/{profissional}
 Route::patch('/profissionais/{profissional}', [ProfissionalController::class, 'update']);
-
-// DELETE http://127.0.0.1:8000/api/profissionais/{profissional}
 Route::delete('/profissionais/{profissional}', [ProfissionalController::class, 'destroy']);
