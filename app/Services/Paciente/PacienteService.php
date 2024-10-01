efef2f59e04ba9c1e9a99e4572882593c2586b84
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-10-01 15:05:13 -0300

    feat: adicionado a função para a criação de usuários

--- app/Services/Paciente/PacienteService.php
+++ app/Services/Paciente/PacienteService.php
@@ -4,12 +4,20 @@ namespace App\Services\Paciente;
 
 use App\Http\Resources\PacienteResource;
 use App\Interfaces\Paciente\PacienteServiceInterface;
+use App\Interfaces\User\UserServiceInterface;
 use App\Models\Paciente;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 
 class PacienteService implements PacienteServiceInterface
 {
+    public function __construct(
+        private UserServiceInterface $userService
+    )
+    {
+
+    }
     public function index()
     {
         try{
@@ -47,7 +55,32 @@ class PacienteService implements PacienteServiceInterface
 
     public function store($data)
     {
-        dd($data);
+
+        $user = $this->userService->store($data);
+        try{
+
+        DB::beginTransaction();
+
+        $paciente = Paciente::create([
+            "id_usuario" => $user['id']
+        ]);
+
+        DB::commit();
+
+        $createdPaciente = Paciente::join("users", "users.id", "=", "pacientes.id_usuario")
+                        ->join('enderecos', 'users.id_endereco', '=', 'enderecos.id_endereco')
+                        ->join('contatos', 'users.id_contato', '=', 'contatos.id_contato')
+                        ->findOrFail($paciente->id_paciente);
+
+        return response()->json(new PacienteResource($createdPaciente), 201);
+
+        } catch(Exception $e){
+            DB::rollback();
+            $data = [
+                "error" => $e->getMessage()
+            ];
+            return response()->json(new PacienteResource($data), 404);
+        }
     }
 
     public function update($data, $id)
