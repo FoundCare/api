59275e630c3c8e950b774122a0c69e57befbd9e8
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-10-07 11:10:51 -0300

    feat: feito a estrutura de especialidadeService

---
+++ app/Services/Especialidade/EspecialidadeService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Especialidade;
+
+use App\Interfaces\Especialidade\EspecialidadeServiceInterface;
+
+class EspecialidadeService implements EspecialidadeServiceInterface
+{
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function store($data)
+    {
+
+    }
+
+    public function update($data, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
