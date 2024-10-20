88a7b171694281f1e5fc0308e5996959aee631ef
Jefferson Matheus <jefferson.oliveira203@etec.sp.gov.br>
2024-10-19 22:13:10 -0300

    feat: adicionado configurações para os tokens de acesso

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::enablePasswordGrant();
-        Passport::hashClientSecrets();
         Passport::tokensExpireIn(Carbon::now()->addDays());
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
         Passport::personalAccessTokensExpireIn(Carbon::now()->addMonths(6));
